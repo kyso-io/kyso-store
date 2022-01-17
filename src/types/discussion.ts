cef82cd7f0ea27733d246239b3418a1848e4377c
Helena <helena@kyso.io>
2022-01-17 17:20:39 +0100

    main report and discussion data structure

---
+++ src/types/discussion.ts
@@ -0,0 +1,25 @@
+import { BaseModel } from './base';
+
+export type Discussion = {
+    name: string;
+    discussion_number: number;
+    type: 'discussion';
+    main: string;
+    number_of_comments: number;
+    tags: string[];
+    description: string;
+    request_private: boolean;
+    is_closed: boolean;
+    is_answered: boolean;
+    created_at: string;
+    updated_at: string;
+    owner_nickname: string; //name under who the report is owned by (name in the url)
+    participants: string[];  // list of participants id (a person who create a comment is a participant)
+    assignees: string[]; //list of member assigned to the discussion
+    author_id: string;
+    comment_ids: [string];
+    team_id: string;
+    report_id: string;
+} & BaseModel;
+
+
--- src/types/report.ts
+++ src/types/report.ts
@@ -2,9 +2,12 @@ import { BaseModel } from './base';
 
 export type Report = {
   name: string;
+  url_name: string;
+  main: string; //main file rendered
   type: 'report';
   views: number;
   stars: number;
+  stargazers: object; // list of people who have started the report
   number_of_comments: number;
   analytics: any;
   provider: any;
@@ -13,7 +16,17 @@ export type Report = {
   tags: string[];
   description: string;
   request_private: boolean;
-  user_id: string;
+  has_metadata: boolean;
+  preview: string; //preview picture
+  default_code: string; //hidden, shown, both
+  created_at: string;
+  updated_at: string;
+  branches_url: string;
+  commits_url: string;
+  tree_url: string;
+  owner_nickname: string; //name under who the report is owned by (name in the url)
+  auhtor_id: string[];  //It can have more than one author
   comment_ids: [string];
   team_id: string;
+  report_id: string;
 } & BaseModel;
