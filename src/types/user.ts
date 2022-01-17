f933d70250be0b73356d5ca2343b53393f59c046
Helena <helena@kyso.io>
2022-01-17 17:48:25 +0100

    user structure data

--- src/types/user.ts
+++ src/types/user.ts
@@ -8,13 +8,18 @@ export type User = {
   username: string;
   nickname: string;
   provider: LoginProviderEnum;
+  github_id: string; // not sure if this is longer needit
   bio: string;
+  location: string;
+  link: string;
+  subscription_id: string;
   plan: string;
   avatar_url: string;
   email_verified: boolean;
   global_permissions: string[];
   hashed_password: string;
-  accessToken: string;
+  access_token: string;
   _email_verify_token?: string;
   accounts: UserAccount[];
+  team_id: string[]; // dont know if this information will be in global permission
 } & BaseModel;
