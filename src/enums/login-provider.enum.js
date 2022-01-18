83b1d4a5e8e0986c84d7197d83da02cf6e5c87da
Eoin Murray <eoinmurray@Eoins-MacBook-Pro.local>
2022-01-18 12:11:44 +0100

    removed hanging js files

--- src/enums/login-provider.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.LoginProviderEnum = void 0;
-var LoginProviderEnum;
-(function (LoginProviderEnum) {
-    LoginProviderEnum["KYSO"] = "kyso";
-    LoginProviderEnum["GOOGLE"] = "google";
-    LoginProviderEnum["GITHUB"] = "github";
-})(LoginProviderEnum = exports.LoginProviderEnum || (exports.LoginProviderEnum = {}));
--- src/enums/team-visibility.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.TeamVisibilityEnum = void 0;
-var TeamVisibilityEnum;
-(function (TeamVisibilityEnum) {
-    TeamVisibilityEnum["PUBLIC"] = "public";
-    TeamVisibilityEnum["PROTECTED"] = "protected";
-    TeamVisibilityEnum["PRIVATE"] = "private";
-})(TeamVisibilityEnum = exports.TeamVisibilityEnum || (exports.TeamVisibilityEnum = {}));
