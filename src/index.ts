4323953115c9aa9c299e41f569b39f7f0137797b
Fran Barrena <francisco@kyso.io>
2022-01-20 13:15:29 +0100

    configurable log level via ENV variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 import { Logger } from "tslog";
 
-export const LOGGER: Logger = new Logger();
+export const LOGGER: Logger = new Logger({
+  minLevel: process.env.TSLOG_MIN_LEVEL ? process.env.TSLOG_MIN_LEVEL as any : "info"
+});
 
 dotenv.config({
   path: `${__dirname}/../.env`,
