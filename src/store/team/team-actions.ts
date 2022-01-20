34c940110a4f580fd149c98bf0328cc8ac98ba70
Daniel Andújar <daniandujar@gmail.com>
2022-01-20 12:54:49 +0100

    feat: team actions

--- src/store/team/team-actions.ts
+++ src/store/team/team-actions.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosResponse } from 'axios';
 import httpClient from '../../services/http-client';
 
-export const fetchTeamByNameAction = createAsyncThunk('team/fetchTeamByName', async (teamName: string) => {
+export const fetchTeamByNameAction = createAsyncThunk('team/fetchTeamByName', async (teamName: string): Promise<Team | null> => {
   try {
     const url = `/teams/${teamName}`;
     const axiosResponse: AxiosResponse<NormalizedResponse<Team>> = await httpClient.get(url, {
@@ -20,7 +20,7 @@ export const fetchTeamByNameAction = createAsyncThunk('team/fetchTeamByName', as
   }
 });
 
-export const fetchTeamByIdAction = createAsyncThunk('team/fetchTeamById', async (id: string) => {
+export const fetchTeamByIdAction = createAsyncThunk('team/fetchTeamById', async (id: string): Promise<Team | null> => {
   try {
     const url = `/teams/id/${id}`;
     const axiosResponse: AxiosResponse<NormalizedResponse<Team>> = await httpClient.get(url);
@@ -35,7 +35,7 @@ export const fetchTeamByIdAction = createAsyncThunk('team/fetchTeamById', async 
   }
 });
 
-export const createTeamAction = createAsyncThunk('team/createTeam', async (team: Team) => {
+export const createTeamAction = createAsyncThunk('team/createTeam', async (team: Team): Promise<Team | null> => {
   try {
     const url = `/teams`;
     const axiosResponse: AxiosResponse<NormalizedResponse<Team>> = await httpClient.post(url, team);
@@ -50,7 +50,7 @@ export const createTeamAction = createAsyncThunk('team/createTeam', async (team:
   }
 });
 
-export const deleteTeamAction = createAsyncThunk('team/deleteTeam', async (id: number) => {
+export const deleteTeamAction = createAsyncThunk('team/deleteTeam', async (id: number): Promise<Team | null> => {
   try {
     const url = `/teams/${id}`;
     const axiosResponse: AxiosResponse<NormalizedResponse<Team>> = await httpClient.delete(url);
@@ -65,12 +65,42 @@ export const deleteTeamAction = createAsyncThunk('team/deleteTeam', async (id: n
   }
 });
 
-export const fetchTeamMembersAction = createAsyncThunk('team/fetchTeamMembers', async (teamName: string) => {
+export const fetchTeamMembersAction = createAsyncThunk('team/fetchTeamMembers', async (teamName: string): Promise<TeamMember[]> => {
   try {
     const url = `/teams/${teamName}/members`;
     const axiosResponse: AxiosResponse<NormalizedResponse<TeamMember[]>> = await httpClient.get(url, {
       headers: { 'x-kyso-team': teamName },
     });
+    if (axiosResponse?.data?.data) {
+      return axiosResponse.data.data;
+    } else {
+      return [];
+    }
+  } catch (e: any) {
+    console.error(`${e.response.status} ${e?.response.statusText}`);
+    return [];
+  }
+});
+
+export const checkMemberBelongsToTheTeamAction = createAsyncThunk('team/checkMemberBelongsToTheTeam', async (payload: { teamId: string; userId: string }): Promise<boolean> => {
+  try {
+    const url = `/teams/${payload.teamId}/members/${payload.userId}`;
+    const axiosResponse: AxiosResponse<NormalizedResponse<boolean>> = await httpClient.post(url, payload);
+    if (axiosResponse?.data?.data) {
+      return axiosResponse.data.data;
+    } else {
+      return false;
+    }
+  } catch (e: any) {
+    console.error(`${e.response.status} ${e?.response.statusText}`);
+    return false;
+  }
+});
+
+export const updateTeamAction = createAsyncThunk('team/updateTeamAction', async (team: Team): Promise<Team | null> => {
+  try {
+    const url = `/teams/${team.id}`;
+    const axiosResponse: AxiosResponse<NormalizedResponse<Team>> = await httpClient.patch(url, team);
     if (axiosResponse?.data?.data) {
       return axiosResponse.data.data;
     } else {
@@ -82,6 +112,17 @@ export const fetchTeamMembersAction = createAsyncThunk('team/fetchTeamMembers', 
   }
 });
 
-export const fetchUsersTeamAction = createAsyncThunk('team/fetchUsersTeam', async () => {
-  return [];
+export const checkTeamNameIsUniqueAction = createAsyncThunk('team/checkTeamNameIsUnique', async (teamName: string): Promise<boolean> => {
+  try {
+    const url = `/teams/check-name/${teamName}`;
+    const axiosResponse: AxiosResponse<NormalizedResponse<boolean>> = await httpClient.get(url);
+    if (axiosResponse?.data?.data) {
+      return axiosResponse.data.data;
+    } else {
+      return false;
+    }
+  } catch (e: any) {
+    console.error(`${e.response.status} ${e?.response.statusText}`);
+    return false;
+  }
 });
