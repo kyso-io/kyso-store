a46e6f1865a3c37a9c7b92f7f3cfb3e4931437d1
Daniel Andújar <daniandujar@gmail.com>
2022-01-21 08:09:11 +0100

    feat: loggin action

--- src/store/auth/auth-actions.ts
+++ src/store/auth/auth-actions.ts
@@ -1,18 +1,26 @@
 import { NormalizedResponseDTO } from '@kyso-io/kyso-model';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosResponse } from 'axios';
+import { LOGGER } from '../..';
 import httpClient from '../../services/http-client';
+import { setError } from '../error/error-slice';
 
-export const loginAction = createAsyncThunk('auth/login', async (credentials: { username: string; password: string; provider: string }) => {
+export const loginAction = createAsyncThunk('auth/login', async (credentials: { username: string; password: string; provider: string }, { dispatch }) => {
   try {
-    const axiosResponse: AxiosResponse<NormalizedResponseDTO<string>> = await httpClient.post('/auth/login', { ...credentials });
-
+    LOGGER.silly('loginAction invoked');
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/login`;
+    LOGGER.silly(`loginAction - POST ${url}`);
+    const axiosResponse: AxiosResponse<NormalizedResponseDTO<string>> = await httpClient.post(url, { ...credentials });
     if (axiosResponse?.data?.data) {
+      LOGGER.silly(`loginAction: axiosResponse ${axiosResponse.data.data}`);
       return axiosResponse.data.data;
     } else {
+      LOGGER.silly(`loginAction: Response didn't have data, returning null`);
       return null;
     }
-  } catch (e) {
+  } catch (e: any) {
+    LOGGER.error(`loginAction: Error processing action: ${e.toString()}`);
+    dispatch(setError(e.toString()));
     return null;
   }
 });
