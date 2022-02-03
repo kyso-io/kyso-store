a5c6a6c47d3de5b7200e2cea3c3a8864f7df6a8b
Fran Barrena <francisco@kyso.io>
2022-02-03 18:28:01 +0100

    first test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import dotenv from 'dotenv';
 import { Logger } from "tslog";
+export const KYSO_CONFIG = require('process.env.NEXT_PUBLIC_CONFIG_FILE_PATH');
+
+console.log(`Reading configuration from: ${process.env.NEXT_PUBLIC_CONFIG_FILE_PATH}`)
+console.log(KYSO_CONFIG)
 
 export const LOGGER: Logger = new Logger({
-  minLevel: process.env.TSLOG_MIN_LEVEL ? process.env.TSLOG_MIN_LEVEL as any : "info"
+  minLevel: KYSO_CONFIG?.kyso_store?.ts_log_level ? KYSO_CONFIG.kyso_store.ts_log_level as any : "silly"
 });
 
 dotenv.config({
   path: `${__dirname}/../.env`,
 });
 
-console.log(__dirname)
-console.log(process.env.NEXT_PUBLIC_CONFIG_FILE_PATH)
 
 export * from './store';
