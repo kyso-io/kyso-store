7f7e2efaaee57b9696a7c7ec32381ff7573552be
Fran Barrena <francisco@kyso.io>
2022-02-03 18:33:37 +0100

    other try

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 import { Logger } from "tslog";
-export const KYSO_CONFIG = require('process.env.NEXT_PUBLIC_CONFIG_FILE_PATH');
+const fs = require('fs');
+
+const configFilePath = process.env.NEXT_PUBLIC_CONFIG_FILE_PATH;
+export const KYSO_CONFIG = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
 
 console.log(`Reading configuration from: ${process.env.NEXT_PUBLIC_CONFIG_FILE_PATH}`)
 console.log(KYSO_CONFIG)
