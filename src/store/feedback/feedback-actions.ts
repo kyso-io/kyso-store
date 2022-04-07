173991b8e48363503dc7ff77f776d402166759d3
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-04-07 09:11:21 +0200

    feat: feebackAction

---
+++ src/store/feedback/feedback-actions.ts
@@ -0,0 +1,19 @@
+import { FeedbackDto, NormalizedResponseDTO, ReportDTO } from '@kyso-io/kyso-model';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AxiosResponse } from 'axios';
+import { RootState } from '..';
+import { buildAuthHeaders, getAPIBaseURL } from '../../helpers/axios-helper';
+import httpClient from '../../services/http-client';
+
+export const newFeedbackAction = createAsyncThunk('reports/newFeedback', async (feedbackDto: FeedbackDto, { getState }): Promise<ReportDTO | null> => {
+  const { auth } = getState() as RootState;
+  const url = `${getAPIBaseURL()}/feedback`;
+  const axiosResponse: AxiosResponse<NormalizedResponseDTO<ReportDTO>> = await httpClient.post(url, feedbackDto, {
+    headers: buildAuthHeaders(auth),
+  });
+  if (axiosResponse?.data?.data) {
+    return axiosResponse.data.data;
+  } else {
+    return null;
+  }
+});
--- src/store/index.ts
+++ src/store/index.ts
@@ -55,6 +55,7 @@ export * from './comments/comments-slice';
 export * from './discussions/discussions-actions';
 export * from './discussions/discussions-slice';
 export * from './error/error-slice';
+export * from './feedback/feedback-actions';
 export * from './full-search/full-search-actions';
 export * from './github/github-actions';
 export * from './github/github-repositories-slice';
