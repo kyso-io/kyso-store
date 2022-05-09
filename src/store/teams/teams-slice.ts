27857658108fe9d859e68586a530b7d28194c01f
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-05-09 14:29:23 +0200

    feat: getOrgAndTeamGivenSluglifiedOrgAndTeam

--- src/store/teams/teams-slice.ts
+++ src/store/teams/teams-slice.ts
@@ -1,4 +1,5 @@
-import { ActionWithPayload, Relations, Team } from '@kyso-io/kyso-model';
+/* eslint-disable no-prototype-builtins */
+import { ActionWithPayload, Organization, Relations, Team } from '@kyso-io/kyso-model';
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '..';
 import slugify from '../../helpers/slugify';
@@ -70,9 +71,39 @@ export const selectTeamBySlugifiedName = (state: RootState, name: string) => {
   if (!name) {
     return null;
   }
-  return Object.values(state.teams.entities!).find((team: any/*{ name: string; sluglified_name: string }*/) => {
+  return Object.values(state.teams.entities!).find((team: any /*{ name: string; sluglified_name: string }*/) => {
     return (team?.name && team.name !== '' && slugify(team.name) === name) || team?.sluglified_name === name;
   });
+};
+
+export const getOrgAndTeamGivenSluglifiedOrgAndTeam = (state: RootState, orgSlug: string, teamSlug: string) => {
+  let organization: Organization | null = null;
+  let team: Team | null = null;
+  if (!orgSlug || orgSlug.length === 0 || !teamSlug || teamSlug.length === 0) {
+    return { organization, team };
+  }
+  for (const organizationId in state.organizations.entities) {
+    if (state.organizations.entities.hasOwnProperty(organizationId)) {
+      const tmpOrganization: any = state.organizations.entities[organizationId];
+      if (tmpOrganization?.sluglified_name === orgSlug || tmpOrganization?.name === orgSlug) {
+        organization = tmpOrganization;
+        break;
+      }
+    }
+  }
+  if (!organization) {
+    return { organization, team };
+  }
+  for (const teamId in state.teams.entities) {
+    if (state.teams.entities.hasOwnProperty(teamId)) {
+      const tmpTeam: any = state.teams.entities[teamId];
+      if ((tmpTeam?.sluglified_name === teamSlug || tmpTeam?.name === teamSlug) && organization.id === tmpTeam.organization_id) {
+        team = tmpTeam;
+        break;
+      }
+    }
+  }
+  return { organization, team };
 };
 
 export default teamsSlice.reducer;
