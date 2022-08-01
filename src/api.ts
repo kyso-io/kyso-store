df5fa60e9095db4e24ea28e4ab2496a0e7abe7e4
Daniel Andújar Lorenzo <daniandujar@gmail.com>
2022-08-01 13:25:10 +0200

    fix: updateOrganizationMemberRoles wrong param type

--- src/api.ts
+++ src/api.ts
@@ -586,7 +586,7 @@ export class Api {
     return axiosResponse.data;
   }
 
-  public async updateOrganizationMemberRoles(organizationId: string, updateOrganizationMembersDTO: UpdateOrganizationMembersDTO[]): Promise<NormalizedResponseDTO<OrganizationMember[]>> {
+  public async updateOrganizationMemberRoles(organizationId: string, updateOrganizationMembersDTO: UpdateOrganizationMembersDTO): Promise<NormalizedResponseDTO<OrganizationMember[]>> {
     const url = `/organizations/${organizationId}/members-roles`;
     const axiosResponse: AxiosResponse<NormalizedResponseDTO<OrganizationMember[]>> = await this.httpClient.post(url, updateOrganizationMembersDTO);
     return axiosResponse.data;
